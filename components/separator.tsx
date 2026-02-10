67aa4ed28291b599ca4f5b8bbbd3786968020c80
Lucas Audoubert <lucas.audoubert@edu.devinci.fr>
2026-02-10 12:27:39 +0100

    Changed the separator component as export function. (For reusability

--- components/separator.tsx
+++ components/separator.tsx
@@ -1,6 +1,9 @@
-{
-  /* Section Separator */
+export function Separator() {
+  return (
+    <div className="relative h-20 overflow-hidden">
+      <div className="absolute inset-0 bg-gradient-to-b from-transparent via-mint/10 to-transparent"></div>
+    </div>
+  );
 }
-<div className="relative h-20 overflow-hidden">
-  <div className="absolute inset-0 bg-gradient-to-b from-transparent via-mint/10 to-transparent"></div>
-</div>;
+
+// CETTE FONCTION EST UNIQUEMENT LA POUR FAIRE JOLIE!!!! OUI VRAIMENT
